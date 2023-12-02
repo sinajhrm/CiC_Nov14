bbed54b120ce116490a5df720331ac9e4d8f0eb1
sinajhrm <sina.jhrm@gmail.com>
2023-12-02 11:22:49 -0500

    +First Task Completed +Changed Folder Structure of react app

---
+++ book-list-app/src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <>
+            <div style={{ padding: "25px", textAlign:"center" }}>
+                <h1>Hello World!</h1>
+                <p>We are going to implement a web app that contains a list for keeping record of your favorite books!
+                    <br />
+                    Awesome, you think?!
+                    <br />
+                    let's do it!
+                </p>
+            </div>
+        </>
+    )
+}

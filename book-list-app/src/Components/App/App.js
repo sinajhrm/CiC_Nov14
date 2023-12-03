6d44d7b6dfb6b3df1c41eaca03ea41c4e867fd60
sinajhrm <sina.jhrm@gmail.com>
2023-12-02 21:49:45 -0500

    +Using props to populate the BookList component

--- book-list-app/src/Components/App/App.js
+++ book-list-app/src/Components/App/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import BookList from "../BookList/BookList";
 
 export default function App() {
+
+    const books = [
+        { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+        { id: 2, title: 'The Fault in Our Stars', author: 'John Green' },
+        { id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee' }
+    ]
+
     return (
-        <>
-            <BookList></BookList>
-        </>
+        <BookList books={books}></BookList>
     )
 }
--- book-list-app/src/Components/BookList/BookList.js
+++ book-list-app/src/Components/BookList/BookList.js
@@ -1,33 +1,30 @@
 import React from "react";
 
-export default function BookList() {
+/**
+ * 
+ * @param {[]} books An array of books
+ * @returns 
+ */
+export default function BookList({ books }) {
     return (
         <div>
             <h2>Book List</h2>
             <table>
                 <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Book Title</th>
+                    <tr key="table-header">
+                        <th>ID</th>
+                        <th>Title</th>
                         <th>Author</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>The Great Gatsby</td>
-                        <td>F. Scott Fitzgerald</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>The Fault in Our Stars</td>
-                        <td>John Green</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>To Kill a Mockingbird</td>
-                        <td>Harper Lee</td>
-                    </tr>
+                    {books.map((book) => (
+                        <tr key={book.id}>
+                            <td>{book.id}</td>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
